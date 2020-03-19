8748cd42bf7614cc6a62971037ca2ff05635602c
amirgamil <amir.amgad01@gmail.com>
2020-03-19 12:17:04 +0400

    Fixed JSON File

---
+++ create_graph_network.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import itertools
+import json
+
+
+data = json.loads("data.json")
+print(data)
