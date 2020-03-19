ebf3c4cfa54cd317150fe37d6ac956d0a0e88b51
amirgamil <amir.amgad01@gmail.com>
2020-03-19 12:20:36 +0400

    Fixed bug reading JSON file

--- create_graph_network.py
+++ create_graph_network.py
@@ -1,12 +1,10 @@
 import pandas as pd
-import matplotlib
-matplotlib.use("Agg")
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 import itertools
 import json
 
 
-data = json.loads("data.json")
+with open("data.json") as f:
+    data = json.load(f)
 print(data)
